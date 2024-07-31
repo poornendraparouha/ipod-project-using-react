4143444cb2782c32a78278cf6dc8c4df02f9eac6
Poornendra Parouha <poornendraparouha@gmail.com>
2024-07-31 11:56:44 +0530

    initialised project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Getting Started with Create React App
+
+#ipod project using create react app
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1>Ipod project</h1>
+  )
+}
+
+export default App;
